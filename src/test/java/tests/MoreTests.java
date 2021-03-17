9a9bc5a2b08e82a705c04dbc8bc14514a09c261f
Vasiliy Golban <vgolban@accesssoftek.com>
2021-03-17 21:04:23 +0200

    Automated manual test from allure testops

--- src/test/java/tests/MoreTests.java
+++ src/test/java/tests/MoreTests.java
@@ -1,10 +1,13 @@
 package tests;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
 
 @Feature("login and logout tests")
 public class MoreTests extends BaseTest{
@@ -12,12 +15,28 @@ public class MoreTests extends BaseTest{
     @Test
     @Tag("web")
     void NewTest1() {
-        homePage.menuSections.shouldHaveSize(6);
+        step("Verify menu sections count", () -> {
+            homePage.menuSections.shouldHaveSize(6);
+        });
     }
 
     @Test
     @Tag("web")
     void NewTest2() {
-        homePage.searchField.should(exist);
+        step("Verify search field exists", () -> {
+            homePage.searchField.should(exist);
+        });
+    }
+
+    @Test
+    @AllureId("1963")
+    @Tag("web")
+    void SushiPageTitle() {
+        step("Open Sushi page", () -> {
+            $(".icon-sushi").click();
+        });
+        step("Verify if title text is 'sushi sets'", () -> {
+            $(".content-subhead__title").shouldHave(text("sushi sets"));
+        });
     }
 }
